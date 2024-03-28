9f9d32267cd5f7ab26bfb09fef854ffa6e993953
Wali Ullah <wali190939@gmail.com>
2024-03-28 19:10:59 +0600

    refractor: search page updated

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,5 +1,38 @@
-const SearchPage = () => {
-  return <div>this is a search page</div>;
+import { redirect } from "next/navigation";
+import Categories from "./_components/categories";
+import { auth } from "@clerk/nextjs";
+import { db } from "@/lib/db";
+import { getCourses } from "@/actions/get-courses";
+import CourseList from "./_components/course-list";
+import SearchInput from "../../_components/search-input";
+
+interface searchPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+}
+const SearchPage = async ({ searchParams }: searchPageProps) => {
+  const { userId } = auth();
+  if (!userId) redirect("/");
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+  const courses = await getCourses({ userId, ...searchParams });
+
+  return (
+    <>
+      <div className="block md:hidden px-6 pt-6 md:mb-0 ">
+        <SearchInput />
+      </div>
+      <div className="space-y-6 p-6">
+        <Categories items={categories} />
+        <CourseList items={courses} />
+      </div>
+    </>
+  );
 };
 
 export default SearchPage;
