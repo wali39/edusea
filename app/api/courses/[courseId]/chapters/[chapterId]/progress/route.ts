186f925a6635b53d9c326d255be391bb859d2717
Wali Ullah <wali190939@gmail.com>
2024-04-21 11:33:20 +0600

    add: course progress and progressbar

---
+++ app/api/courses/[courseId]/chapters/[chapterId]/progress/route.ts
@@ -0,0 +1,35 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function PUT(
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const { isCompleted } = await req.json();
+    if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+
+    const userProgress = await db.userProgress.upsert({
+      where: {
+        userId_chapterId: {
+          userId,
+          chapterId: params.chapterId,
+        },
+      },
+      update: {
+        isComplete: isCompleted,
+      },
+      create: {
+        userId,
+        chapterId: params.chapterId,
+        isComplete: isCompleted,
+      },
+    });
+    return NextResponse.json(userProgress);
+  } catch (error) {
+    console.log("USER_PROGRESS", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
