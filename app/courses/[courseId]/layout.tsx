01ced92bca35b4cfed5b72c0f35b8f24ce83e7e3
Wali Ullah <wali190939@gmail.com>
2024-04-21 11:47:34 +0600

    refractor: courses page updated

--- app/courses/[courseId]/layout.tsx
+++ app/courses/[courseId]/layout.tsx
@@ -1,7 +1,55 @@
-const CourseLayout = ({ children }: { children: React.ReactNode }) => {
+import { getUserProgress } from "@/actions/get-user-progress";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import CourseSidebar from "./_components/course-sidebar";
+import CourseNavbar from "./_components/course-navbar";
+
+const CourseLayout = async ({
+  children,
+  params,
+}: {
+  params: { courseId: string };
+  children: React.ReactNode;
+}) => {
+  const { userId } = auth();
+  if (!userId) redirect("/");
+
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+    include: {
+      chapters: {
+        where: {
+          isPublished: true,
+        },
+        include: {
+          userProgress: {
+            where: {
+              userId,
+            },
+          },
+        },
+        orderBy: {
+          position: "asc",
+        },
+      },
+    },
+  });
+  if (!course) redirect("/");
+  const progressCount = await getUserProgress(userId, course.id);
+
   return (
-    
-      <div>{children}</div>
+    <div className="h-full">
+      <div className="md:pl-80 h-[80px] fixed w-full inset-y-0 z-50">
+        <CourseNavbar course={course} progressCount={progressCount} />
+      </div>
+      <div className="hidden md:flex flex-col w-80 h-full fixed inset-y-0  z-50 ">
+        <CourseSidebar course={course} progressCount={progressCount} />
+      </div>
+      <main className="md:pl-80 pt-[80px] h-full">{children}</main>;
+    </div>
   );
 };
 
--- app/courses/[courseId]/page.tsx
+++ app/courses/[courseId]/page.tsx
@@ -1,5 +1,24 @@
-const CourseIdPage = () => {
-  return <div>course id page</div>;
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+      isPublished: true,
+    },
+    include: {
+      chapters: {
+        where: {
+          isPublished: true,
+        },
+      },
+    },
+  });
+  if (!course) redirect("/");
+  return redirect(
+    `/courses/${params.courseId}/chapters/${course.chapters[0].id}`
+  );
 };
 
 export default CourseIdPage;
