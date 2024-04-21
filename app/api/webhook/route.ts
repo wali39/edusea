85b99ed9b4e536cf998dea6d25ec34f826759bd9
Wali Ullah <wali190939@gmail.com>
2024-04-21 11:27:46 +0600

    add: stripe payment

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,42 @@
+import { db } from "@/lib/db";
+import { stripe } from "@/lib/stripe";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+export  async function POST(req: Request) {
+  const body = await req.text();
+  const signature = headers().get("Stripe-Signature") as string;
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+  } catch (error: any) {
+    return new NextResponse("Webhook error", { status: 400 });
+  }
+  const session = event.data.object as Stripe.Checkout.Session;
+  const userId = session?.metadata?.userId;
+  const courseId = session?.metadata?.courseId;
+
+  if (event.type == "checkout.session.completed") {
+    if (!userId || !courseId) {
+      return new NextResponse(`Webhook Error: Missing metadata`, {
+        status: 400,
+      });
+    }
+    await db.purchase.create({
+      data: {
+        userId,
+        courseId,
+      },
+    });
+  } else {
+    return new NextResponse(`Unhandled event: ${event.type}`, { status: 200 });
+  }
+  return new NextResponse(null, { status: 200 });
+}
