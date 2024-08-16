a9651202ebdff913df61bef3eb47405659ce4e43
Wali Ullah <wali190939@gmail.com>
2024-08-17 05:23:27 +0600

    add: delete courses developement

---
+++ scripts/clear-courses.ts
@@ -0,0 +1,14 @@
+// const { PrismaClient } = require("@prisma/client");
+// const db = new PrismaClient();
+
+// async function ClearCourse() {
+//   try {
+//     await db.course.deleteMany();
+//     console.log("courses deleted successfully!");
+//   } catch (error) {
+//     console.log("something wrong happend!");
+//   } finally {
+//     await db.$disconnect();
+//   }
+// }
+// ClearCourse();
